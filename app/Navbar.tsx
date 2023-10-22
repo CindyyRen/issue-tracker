47320a1a4a13d1226dc3d355955b862d20ae88e3
Cindy Ren <bdcindy78@gmail.com>
2023-10-22 13:42:53 +1100

    prisma-setup

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const Navbar = () => {
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
   ];
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -16,13 +19,16 @@ const Navbar = () => {
         {links.map((item) => (
           <Link
             key={item.label}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              'text-zinc-900': item.href === currentPath,
+              'text-zinc-500': item.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
             href={item.href}
           >
             {item.label}
           </Link>
         ))}
-
       </ul>
     </nav>
   );
