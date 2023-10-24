69d0797675b856e868d43d4fb11a001922ff3996
Cindy Ren <bdcindy78@gmail.com>
2023-10-24 18:29:20 +1100

    handle-form-submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,17 +3,40 @@
 import React from 'react';
 import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+  title: String;
+  description: String;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  // register is used for registering input elements within your form.
+  //  control: date pickers, custom dropdowns, or "SimpleMDE" text editor.
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const onSubmit = async (data: IssueForm) => {
+    await axios.post('/api/issues', data);
+    router.push('/issues');
+  };
   return (
-    <div className="max-w-xl space-y-3">
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register('title')} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        // field object contains various props (e.g., value, onChange, onBlur)
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
