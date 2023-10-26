381b7a724b68c105525d021cc35387c82f253d75
Cindy Ren <bdcindy78@gmail.com>
2023-10-26 15:41:05 +1100

    Refactor the nav bar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [{ key: 'referrer-policy', value: 'no-referrer' }],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
