f184f2de9958368811721cc58027c06c034d1a7e
Cindy Ren <bdcindy78@gmail.com>
2023-10-27 19:34:04 +1100

    pagenation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import Image from 'next/image';
-import IssuesPage from './issues/list/page';
+// import Image from 'next/image';
+// import IssuesPage from './issues/list/page';
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <>
+//       <IssuesPage />
+//     </>
+//   );
+// }
+import Pagination from './components/Pagination';
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
-    <>
-      <IssuesPage />
-    </>
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
   );
 }
