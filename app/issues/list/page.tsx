b824bd32b4c77dfb90baa1c9be2ac77a90452402
Cindy Ren <bdcindy78@gmail.com>
2023-10-27 11:42:01 +1100

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,12 +1,27 @@
 import prisma from '@/prisma/client';
-import delay from 'delay';
 import { Table } from '@radix-ui/themes';
 import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+
+  const status = statuses.includes(searchParams?.status)
+    ? searchParams.status
+    : undefined;
+  // const status = undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
+
   return (
     <div>
       <IssueActions />
